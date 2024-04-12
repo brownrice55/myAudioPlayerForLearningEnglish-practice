bb223c41a0f1a339db5e3658f79b301593a7b2ff
brownrice55 <113149599+brownrice55@users.noreply.github.com>
2024-04-12 20:06:01 +0900

    feat: add a function to designate numbers

--- js/src/js/audio.js
+++ js/src/js/audio.js
@@ -11,6 +11,7 @@
     this.isSettingsOpen = true;
     this.settingsElm = document.querySelector('.js-settings');
     this.settingsRadioElm = this.settingsElm.querySelectorAll('input[type="radio"]');
+    this.settingsShowInputRadioElm = this.settingsElm.querySelectorAll('.js-showInputRadio');
 
     this.settingsSelectElm = this.settingsElm.querySelectorAll('select');
     this.settingsSelectNumElm = this.settingsSelectElm[0];
@@ -35,8 +36,11 @@
     this.fileName1 = this.settingsData.filename1 || '';
     this.fileName2 = this.settingsData.filename2 || '';
     this.num = this.settingsData.no || 1;
+    this.numArray = this.settingsData.array || 1;
+    this.numType = this.settingsData.type || 1;
     this.endNum = 1000;
     this.cnt = 0;
+    this.arrayIndex = 0;
     this.repeat = this.settingsData.repeat || 1;
     this.acceleration = this.settingsData.acceleration || 0;
     this.speedInit = this.settingsData.speed || 1;
@@ -64,14 +68,21 @@
     this.settingsSelectRepeatElm.value = this.repeat;
     this.settingsSelectSpeedElm.value = this.speedInit;
     this.settingsSelectAccelerationElm.value = this.acceleration;
+    this.settingsInputNumElm.value = this.numArray;
     this.settingsInputPathElm.value = this.path;
     this.settingsSelectDigitElm.value = this.digit;
     this.settingsInputFileName1Elm.value = this.fileName1;
     this.settingsInputFileName2Elm.value = this.fileName2;
+
+    let indexArray = (this.numType===1) ? [0,1]: [1,0];
+    this.settingsRadioElm[indexArray[0]].checked = true;
+    this.settingsRadioElm[indexArray[1]].checked = false;
+    this.settingsShowInputRadioElm[indexArray[0]].classList.remove('disp--none');
+    this.settingsShowInputRadioElm[indexArray[1]].classList.add('disp--none');
   };
 
   AudioPlayer.prototype.selectType = function(e) {
-    let index = e.target.dataset.index;
+    let index = e.target.value;
     let showInputRadioElms = document.querySelectorAll('.js-showInputRadio');
     showInputRadioElms.forEach((elm) => {
       if(elm.dataset.index===index) {
@@ -83,12 +94,12 @@
     });
   };
 
-  AudioPlayer.prototype.getShowElm = function() {
-    return '現在再生中の問題の番号：' + this.num + '<br>残り：' + (this.repeat-this.cnt) + '/' + this.repeat + '回<br>スピード：' + this.speed + '秒<br>加速：' + this.acceleration + '秒';
+  AudioPlayer.prototype.getShowElm = function(aNum) {
+    return '現在再生中の問題の番号：' + aNum + '<br>残り：' + (this.repeat-this.cnt) + '/' + this.repeat + '回<br>スピード：' + this.speed + '秒<br>加速：' + this.acceleration + '秒';
   };
 
-  AudioPlayer.prototype.saveSettings = function(aNum, aRepeat, aSpeedInit, aAcceleration, aPath, aDigit, aFileName1, aFileName2) {
-    let data = { no:aNum, repeat:aRepeat, speed:aSpeedInit, acceleration: aAcceleration, path:aPath, digit: aDigit, filename1:aFileName1, filename2:aFileName2};
+  AudioPlayer.prototype.saveSettings = function(aNum, aNumArray, aNumType, aRepeat, aSpeedInit, aAcceleration, aPath, aDigit, aFileName1, aFileName2) {
+    let data = { no:aNum, array:aNumArray, type:aNumType, repeat:aRepeat, speed:aSpeedInit, acceleration: aAcceleration, path:aPath, digit: aDigit, filename1:aFileName1, filename2:aFileName2};
     localStorage.setItem('settingsData', JSON.stringify(data));
   };
 
@@ -100,7 +111,15 @@
 
   AudioPlayer.prototype.setNum = function(e) {
     if(this.isSettingsOpen) {//set Settings
-      this.num = this.settingsSelectNumElm.value;
+      if(this.settingsRadioElm[0].checked) {
+        this.num = this.settingsSelectNumElm.value;
+        this.numType = 1;
+      }
+      else {
+        this.numArray = this.settingsInputNumElm.value.split(',');
+        this.num = this.numArray[this.arrayIndex];
+        this.numType = 2;
+      }
       this.repeat = this.settingsSelectRepeatElm.value;
       this.speedInit = parseFloat(this.settingsSelectSpeedElm.value);
       this.speed = this.speedInit;
@@ -110,15 +129,16 @@
       this.fileName1 = this.settingsInputFileName1Elm.value;
       this.fileName2 = this.settingsInputFileName2Elm.value;
   
-      this.saveSettings(this.num, this.repeat, this.speedInit, this.acceleration,this.path, this.digit, this.fileName1, this.fileName2);
+      this.saveSettings(this.num, this.numArray, this.numType, this.repeat, this.speedInit, this.acceleration,this.path, this.digit, this.fileName1, this.fileName2);
 
       this.settingsElm.classList.add('disp--none');
       this.btnElm.innerHTML = '設定を表示する'
       this.isSettingsOpen = !this.isSettingsOpen;
-      this.showElm.innerHTML = this.getShowElm();
 
       this.videoElm.parentNode.className = '';
-      this.setPath();
+      let num = (this.numType===1) ? this.num: this.numArray[0];
+      this.showElm.innerHTML = this.getShowElm(num);
+      this.setPath(num);
       this.videoElm.load();
       this.videoPlayRateControllerSet();
     }
@@ -129,47 +149,69 @@
     }
   };
 
-  AudioPlayer.prototype.getSerialNumber = function() {
+  AudioPlayer.prototype.getSerialNumber = function(aNum) {
     let digitZero = '';
     for(let cnt=0;cnt<this.digit;++cnt) {
       digitZero += '0';
     }
-    return (digitZero + this.num).slice(-this.digit);
+    return (digitZero + aNum).slice(-this.digit);
   };
 
-  AudioPlayer.prototype.setPath = function() {
-    let serialNumber = this.getSerialNumber();
+  AudioPlayer.prototype.setPath = function(aNum) {
+    let serialNumber = this.getSerialNumber(aNum);
     let path = this.path + this.fileName1 + serialNumber + this.fileName2 + '.mp3';
     this.videoElm.innerHTML = '<source src="' + path + '" type="video/mp4">';
   };
 
   AudioPlayer.prototype.videoPlayRateControllerSet = function() {
-    this.videoElm.addEventListener('loadeddata', this.videoPlayRateController1.bind(this));
+    this.videoElm.addEventListener('loadeddata', this.videoPlayRateControllerFirstTime.bind(this));
     this.videoElm.addEventListener('ended', this.videoPlayRateController.bind(this));
   };
 
-  AudioPlayer.prototype.videoPlayRateController1 = function() {
+  AudioPlayer.prototype.videoPlayRateControllerFirstTime = function() {
     this.videoElm.playbackRate = this.speed;
   };
 
   AudioPlayer.prototype.videoPlayRateController = function() {
-    if(this.num>=this.endNum) {
-      videoElm.pause();
-    }
-    else {
-      if(this.cnt>=this.repeat-1) {
-        ++this.num;
-        this.speed = this.speedInit;
-        this.setPath();
-        this.cnt = 0;
+    if(this.numType===1) {
+      if(this.num>=this.endNum) {
+        videoElm.pause();
       }
       else {
-        ++this.cnt;
-        this.speed = this.speedInit + this.cnt*this.acceleration;
-        this.videoElm.playbackRate = this.speed;
+        if(this.cnt>=this.repeat-1) {
+          ++this.num;
+          this.speed = this.speedInit;
+          this.setPath(this.num);
+          this.cnt = 0;
+        }
+        else {
+          ++this.cnt;
+          this.speed = this.speedInit + this.cnt*this.acceleration;
+          this.videoElm.playbackRate = this.speed;
+        }
+        this.showElm.innerHTML = this.getShowElm(this.num);
+        this.videoElm.load();
       }
-      this.showElm.innerHTML = this.getShowElm();
-      this.videoElm.load();
+    }
+    else {
+      if(this.arrayIndex>=(this.numArray.length)) {
+        videoElm.pause();
+      }
+      else {
+        if(this.cnt>=this.repeat-1) {
+          ++this.arrayIndex;
+          this.speed = this.speedInit;
+          this.setPath(this.numArray[this.arrayIndex]);
+          this.cnt = 0;
+        }
+        else {
+          ++this.cnt;
+          this.speed = this.speedInit + this.cnt*this.acceleration;
+          this.videoElm.playbackRate = this.speed;
+        }
+        this.showElm.innerHTML = this.getShowElm(this.numArray[this.arrayIndex]);
+        this.videoElm.load();
+      }
     }
   };
 
